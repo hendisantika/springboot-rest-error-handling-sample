d948c25fb567f9c1c73abab961b1c89dbecf272f
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-17 06:40:14 +0700

    #19 - Add getAll & getById API in UserController

--- src/main/java/com/hendisantika/springbootresterrorhandlingsample/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootresterrorhandlingsample/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.hendisantika.springbootresterrorhandlingsample.controller;
 
+import com.hendisantika.springbootresterrorhandlingsample.exception.ResourceNotFoundException;
+import com.hendisantika.springbootresterrorhandlingsample.model.User;
 import com.hendisantika.springbootresterrorhandlingsample.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Min;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +30,18 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping(value = "/users")
+    List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping(value = "/users/{id}")
+    ResponseEntity<User> getById(@PathVariable("id") @Min(1) int id) {
+
+        User usr = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User with ID :" + id + " Not Found!"));
+
+        return ResponseEntity.ok().body(usr);
+    }
 }

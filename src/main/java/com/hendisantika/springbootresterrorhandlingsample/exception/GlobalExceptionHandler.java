cd37c2e08f4f9604b3b42a527b639b4b24f0019a
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-17 06:33:59 +0700

    #9 - Add handleHttpMediaTypeNotSupported in GlobalExceptionHandler

--- src/main/java/com/hendisantika/springbootresterrorhandlingsample/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hendisantika/springbootresterrorhandlingsample/exception/GlobalExceptionHandler.java
@@ -1,7 +1,18 @@
 package com.hendisantika.springbootresterrorhandlingsample.exception;
 
+import com.hendisantika.springbootresterrorhandlingsample.error.ApiError;
+import com.hendisantika.springbootresterrorhandlingsample.error.ResponseEntityBuilder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,5 +25,28 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
  */
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    // handleHttpMediaTypeNotSupported : triggers when the JSON is invalid
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
+            HttpMediaTypeNotSupportedException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+
+        List<String> details = new ArrayList<String>();
+
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(ex.getContentType());
+        builder.append(" media type is not supported. Supported media types are ");
+        ex.getSupportedMediaTypes().forEach(t -> builder.append(t).append(", "));
+
+        details.add(builder.toString());
+
+        ApiError err = new ApiError(LocalDateTime.now(), HttpStatus.BAD_REQUEST, "Invalid JSON", details);
+
+        return ResponseEntityBuilder.build(err);
+
+    }
 
 }

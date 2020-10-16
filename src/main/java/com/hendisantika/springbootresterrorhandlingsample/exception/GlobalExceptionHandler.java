273568f7a4bc0c13af1da07d1487929a4fa88f78
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-17 06:33:31 +0700

    #8 - Add GlobalExceptionHandler

---
+++ src/main/java/com/hendisantika/springbootresterrorhandlingsample/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.hendisantika.springbootresterrorhandlingsample.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rest-error-handling-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/10/20
+ * Time: 06.33
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+}

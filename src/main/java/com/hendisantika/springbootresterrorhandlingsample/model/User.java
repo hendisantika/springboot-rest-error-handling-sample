dbefd51da97c345d63fb4dbd0ece48a0be01035a
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-17 06:30:09 +0700

    #4 - Add User Model

---
+++ src/main/java/com/hendisantika/springbootresterrorhandlingsample/model/User.java
@@ -0,0 +1,46 @@
+package com.hendisantika.springbootresterrorhandlingsample.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rest-error-handling-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/10/20
+ * Time: 06.28
+ */
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    private String password;
+}

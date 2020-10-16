082f1425107c823c8527fbc9acd0aa993ace48cc
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-17 06:25:55 +0700

    #1 - Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-rest-error-handling-sample
---
+++ src/test/java/com/hendisantika/springbootresterrorhandlingsample/SpringbootRestErrorHandlingSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootresterrorhandlingsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestErrorHandlingSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

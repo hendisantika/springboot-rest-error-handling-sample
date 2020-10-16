f26d4c5e84416a2da426d21188a25b59ffc32125
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-17 06:32:04 +0700

    #6 - Add ApiError

---
+++ src/main/java/com/hendisantika/springbootresterrorhandlingsample/error/ApiError.java
@@ -0,0 +1,30 @@
+package com.hendisantika.springbootresterrorhandlingsample.error;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rest-error-handling-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/10/20
+ * Time: 06.31
+ */
+@Data
+@AllArgsConstructor
+public class ApiError {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+
+    private HttpStatus status;
+    private String message;
+    private List<String> errors;
+}

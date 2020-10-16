61b515377c21f522b7a3f285dfa22829288edcd4
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-17 06:41:10 +0700

    #21 - Add create API in UserController

--- src/main/java/com/hendisantika/springbootresterrorhandlingsample/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootresterrorhandlingsample/controller/UserController.java
@@ -8,11 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
+import java.net.URI;
 import java.util.List;
 
 /**
@@ -53,5 +58,17 @@ public class UserController {
                 .orElseThrow(() -> new ResourceNotFoundException(username + " NOT Found!"));
 
         return ResponseEntity.ok().body(usr);
+    }
+
+    @PostMapping(value = "/users")
+    ResponseEntity<?> create(@Valid @RequestBody User user) {
+
+        User addeduser = userRepository.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(addeduser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
